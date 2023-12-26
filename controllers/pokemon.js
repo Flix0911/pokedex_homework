592813e1b873f6bf06a0f4597035b868d182e5c5
Erick Valencia <evalencia0911@gmail.com>
2023-12-26 12:36:31 -0700

    update and edit functional. edit.ejs is not displaying image correctly, will return

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -11,30 +11,49 @@ const pokemon = require("../models/pokemon.js")
 
 //ROUTES
 
-//INDEX - GET
+//INDEX - GET - /pokemon
 router.get("/", (req, res) => {
     //confirming data is connected. ANSWER: YES, returns JSON data
     //res.send(pokemon)
     res.render("index.ejs", {pokemon})
 })
 
-//NEW - GET
+//NEW - GET - /pokemon/new
+router.get("/new", (req, res) => {
+    res.render("new.ejs")
+})
 
 //DESTORY - DELETE
 
 //UPDATE - PUT
+router.put("/:id", (req, res) => {
+    //get id from params
+    const id = req.params.id
+    //get the body
+    const body = req.body
+    //swap out the old version for the new version
+    pokemon[id] = body
+    res.redirect("/pokemon")
+})
 
 //CREATE - POST
 
-//EDIT - GET
+//EDIT - GET - /pokemon/:id/edit
+router.get("/:id/edit", (req, res) => {
+    //get id from params
+    const id = req.params.id
+    //get pokemon from array
+    const pokemons = pokemon[id]
+    res.render("edit.ejs", {pokemons, id})
+})
 
-//SHOW - GET
+//SHOW - GET - /pokemon/:id
 router.get("/:id", (req, res) => {
     //get id from params
     const id = req.params.id
     //get pokemon from array
     const pokemons = pokemon[id]
-    // res.send(pokemonId)
+    // res.send(pokemons)
     res.render("show.ejs", {pokemons, id})
 })
 
